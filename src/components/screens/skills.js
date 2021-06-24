ed8f5fc615e7ffe27efced9a4ef8e70e2cca5d47
Edward <edward.arechwa@gogomedia.pl>
2021-06-24 11:15:54 +0200

    skills

--- src/components/screens/skills.js
+++ src/components/screens/skills.js
@@ -1,49 +1,65 @@
 import React from "react";
-import { graphql } from "gatsby";
+import { graphql, useStaticQuery } from "gatsby";
 // import { stringSplit } from "../utils";
 
+const query = graphql`
+	query {
+		markdownRemark(fileAbsolutePath: { regex: "/skills.md/" }) {
+			frontmatter {
+				screenTitle
+				infoText
+				skillsList {
+					description
+					level
+					name
+				}
+			}
+		}
+	}
+`;
+
 const Skills = () => {
-	// const splitTitle = stringSplit(data.title);
+	const data = useStaticQuery(query);
+	const { screenTitle, infoText, skillsList } = data.markdownRemark.frontmatter;
 	return (
-		<p>skills</p>
-		// <section className="skills container">
-		// 	<h2 className="title title--main title--skills skills__title">
-		// 		{splitTitle.map((word, index) => {
-		// 			return (
-		// 				<span
-		// 					className={`row  animate__animated animate__fadeInRight animate__delay-${index}`}
-		// 					key={index}
-		// 				>
-		// 					{word.map((letter, index) => (
-		// 						<span className="title__letter title__letter--scale" key={`${letter}-${index}`}>
-		// 							{letter}
-		// 						</span>
-		// 					))}
-		// 				</span>
-		// 			);
-		// 		})}
-		// 	</h2>
-		// 	<ul className="skills-list skills__skills-list">
-		// 		{data.skills.map((item) => (
-		// 			<li
-		// 				className="skills-list__item  animate__animated animate__fadeInLeft"
-		// 				key={`${item.name}${item.level}`}
-		// 			>
-		// 				<p className="skills-list__skill">{item.name}</p>
-		// 				<ul className="dots skills-list__dots  animate__animated animate__fadeInLeft animate__delay-2">
-		// 					{Array.from({ length: 5 }).map((_, index) => (
-		// 						<li
-		// 							className={`dots__item ${index <= item.level ? "dots__item--active" : ""}`}
-		// 							key={index}
-		// 						>
-		// 							<p className="hidden">{item.description}</p>
-		// 						</li>
-		// 					))}
-		// 				</ul>
-		// 			</li>
-		// 		))}
-		// 	</ul>
-		// </section>
+		<section className="screen skills container">
+			<h2 className="title title--main title--skills skills__title">
+				{screenTitle.map((word, index) => {
+					return (
+						<span
+							className={`row  animate__animated animate__fadeInRight animate__delay-${index}`}
+							key={word}
+						>
+							{word.split("").map((letter, index) => (
+								<span className="title__letter title__letter--scale" key={`${letter}-${index}`}>
+									{letter}
+								</span>
+							))}
+						</span>
+					);
+				})}
+			</h2>
+			<ul className="skills__list skills__skills-list">
+				{skillsList.map((skill) => (
+					<li
+						className="skills__item  animate__animated animate__fadeInLeft"
+						key={`${skill.name}${skill.level}`}
+					>
+						<p className="skills__item-name">{skill.name}</p>
+						<ul className="dots skills__dots  animate__animated animate__fadeInLeft animate__delay-2">
+							{Array.from({ length: 5 }).map((_, index) => (
+								<li
+									className={`dots__item ${index <= skill.level ? "dots__item--active" : ""}`}
+									key={index}
+								>
+									<p className="hidden">{skill.description}</p>
+								</li>
+							))}
+						</ul>
+					</li>
+				))}
+			</ul>
+		</section>
 	);
 };
 
