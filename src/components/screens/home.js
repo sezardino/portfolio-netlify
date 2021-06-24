b7188300b60a8a8701e4d6417fb34336b9b96919
Edward <edward.arechwa@gogomedia.pl>
2021-06-24 10:42:45 +0200

    home

--- src/components/screens/home.js
+++ src/components/screens/home.js
@@ -1,27 +1,54 @@
-import { graphql } from "gatsby";
+import { graphql, useStaticQuery } from "gatsby";
 import React from "react";
 
+const query = graphql`
+	query {
+		markdownRemark(fileAbsolutePath: { regex: "/home.md/" }) {
+			frontmatter {
+				title
+				screenTitle
+				subtitle
+				image {
+					childImageSharp {
+						fluid(quality: 100) {
+							src
+						}
+					}
+				}
+			}
+		}
+	}
+`;
+
 const Home = () => {
+	const data = useStaticQuery(query);
+	const { image, screenTitle, subtitle, title } = data.markdownRemark.frontmatter;
 	return (
-		<p>home</p>
-		// <section className="hero container animate__animated ">
-		// 	<div className="hero__img animate__animated animate__fadeInBottomRight">
-		// 		{/* <ImageComponent data={imageProps} /> */}
-		// 	</div>
-		// 	<div className="hero__wrapper undefined">
-		// 		<h2 className="title title--hero hero__title">
-		// 			<span className="title title--name hero__title--name animate__delay-12 animate__animated animate__fadeInBottomLeft">
-		// 				{data.name}
-		// 			</span>
-		// 			<span className="title title--surname hero__title--surname animate__animated animate__delay-13 animate__animated animate__fadeInBottomLeft">
-		// 				{data.surname}
-		// 			</span>
-		// 		</h2>
-		// 		<p className="text text--hero hero__text animate__animated animate__delay-14 animate__animated animate__fadeInBottomLeft">
-		// 			{data.position}
-		// 		</p>
-		// 	</div>
-		// </section>
+		<section className="hero container animate__animated ">
+			<h1 className="hidden">{title}</h1>
+			<div className="hero__img animate__animated animate__fadeInBottomRight">
+				<img src={image.childImageSharp.fluid.src} alt={title} />
+			</div>
+			<div className="hero__wrapper">
+				<h2 className="title title--hero hero__title">
+					{screenTitle.map((item, index) => (
+						<span
+							key={item}
+							className={`title title--${
+								index === 0 ? "name" : "surname"
+							} hero__title--name animate__delay-${
+								index === 0 ? "12" : "13"
+							} animate__animated animate__fadeInBottomLeft`}
+						>
+							{item}
+						</span>
+					))}
+				</h2>
+				<p className="text text--hero hero__text animate__animated animate__delay-14 animate__animated animate__fadeInBottomLeft">
+					{subtitle}
+				</p>
+			</div>
+		</section>
 	);
 };
 
