8e4ca42865eb83537085eef8f3c4aff50b32ba63
Edward <edward.arechwa@gogomedia.pl>
2021-06-24 10:42:40 +0200

    about screen, audit scss

--- src/components/screens/about.js
+++ src/components/screens/about.js
@@ -1,34 +1,54 @@
 import React from "react";
-import { graphql } from "gatsby";
+import { graphql, useStaticQuery } from "gatsby";
 // import { stringSplit } from "../utils";
 
+const query = graphql`
+	query MyQuery {
+		markdownRemark(fileAbsolutePath: { regex: "/about.md/" }) {
+			frontmatter {
+				screenTitle
+			}
+			html
+		}
+	}
+`;
+
 const About = () => {
+	const data = useStaticQuery(query);
+	const {
+		frontmatter: { screenTitle },
+		html,
+	} = data.markdownRemark;
+	console.log(screenTitle);
 	return (
-		<p>about</p>
-		// <section className="about container">
-		// 	<h2 className="title title--main title--about about__title">
-		// 		{splitTitle.map((word, index) => {
-		// 			return (
-		// 				<span className={`row  animate__animated animate__rollIn animate__delay-${index}`} key={index}>
-		// 					{word.map((letter) => (
-		// 						<span className="title__letter title__letter--scale" key={`${letter}-${index}`}>
-		// 							{letter}
-		// 						</span>
-		// 					))}
-		// 				</span>
-		// 			);
-		// 		})}
-		// 	</h2>
-		// 	<div className="about__wrapper">
-		// 		<p className="text text--about about__text animate__animated animate__rollIn animate__delay-2">
-		// 			{data.frstText}
-		// 		</p>
-		// 		<br />
-		// 		<p className="text text--about about__text animate__animated animate__rollIn animate__delay-2">
-		// 			{data.scndText}
-		// 		</p>
-		// 	</div>
-		// </section>
+		<section className="about container">
+			<h2 className="title title--main title--about about__title">
+				{screenTitle.map((word, index) => (
+					<span className={`row  animate__animated animate__rollIn animate__delay-${index}`} key={word}>
+						{word.split("").map((letter, index) => (
+							<span className="title__letter title__letter--scale" key={`${letter}-${index}`}>
+								{letter}
+							</span>
+						))}
+					</span>
+				))}
+				{/* {splitTitle.map((word, index) => {
+					return (
+						<span className={`row  animate__animated animate__rollIn animate__delay-${index}`} key={index}>
+							{word.map((letter) => (
+								<span className="title__letter title__letter--scale" key={`${letter}-${index}`}>
+									{letter}
+								</span>
+							))}
+						</span>
+					);
+				})} */}
+			</h2>
+			<div
+				className="about__wrapper text--about animate__animated animate__rollIn animate__delay-2"
+				dangerouslySetInnerHTML={{ __html: html }}
+			></div>
+		</section>
 	);
 };
 
