309fe991ae2a59035f7c2d38e210e8babfaa87e8
Edward <edward.arechwa@gogomedia.pl>
2021-07-02 10:21:31 +0200

    delete images

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,14 +13,6 @@ module.exports = {
         {
             resolve: "gatsby-source-filesystem",
             options: {
-                name: "images",
-                path: "./src/images/",
-            },
-            __key: "images",
-        },
-        {
-            resolve: "gatsby-source-filesystem",
-            options: {
                 name: "pages",
                 path: "./src/pages/",
             },
