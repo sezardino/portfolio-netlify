2722b9fea22a1846e112b0ddaee4e585da89060a
Edward <edward.arechwa@gogomedia.pl>
2021-07-02 11:47:51 +0200

    updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,52 +9,44 @@ const getMenuData = (menu, { index }) => {
 	return { menu, prevPage, nextPage };
 };
 
+const getScreenIndex = (array, slug) => array.findIndex((screen) => screen.slug === slug);
+
 const IndexPage = ({ location, data }) => {
 	const menuList = data.markdownRemark.frontmatter.menuList;
 	const currentPage = {};
 	currentPage.slug = location.hash ? location.hash.replace("#", "") : "home";
 	currentPage.index = menuList.findIndex((item) => item.slug === currentPage.slug);
-	const { nextPage, prevPage, menu } = getMenuData(menuList, currentPage);
+	const menuData = getMenuData(menuList, currentPage);
+	const { nextPage, prevPage, menu } = menuData;
 
 	const screens = menu.map((screen) => ({ ...screen, component: components[`${screen.label}Screen`] }));
 
 	return (
-		<Layout menu={getMenuData(menuList, currentPage)} currentPage={currentPage}>
+		<Layout menu={menuData} currentPage={currentPage}>
 			<div className="screens">
 				{screens.map(({ slug, component: Component }) => {
-					const className =
-						currentPage.slug === slug
-							? "screen--current"
-							: nextPage.slug === slug
-							? "screen--next"
-							: prevPage.slug === slug
-							? "screen--prev"
-							: "";
+					let className;
+					const pageIndex = getScreenIndex(screens, slug);
+					if (currentPage.index === pageIndex) {
+						className = "screen--current";
+					} else if (pageIndex < currentPage.index) {
+						className = "screen--prev";
+					} else if (pageIndex > currentPage.index) {
+						className = "screen--next";
+					}
 					return (
 						<div className={`screen ${className}`} key={slug}>
 							<Component isCurrent={currentPage.slug === slug} />
 						</div>
 					);
 				})}
-				{/* <div className="screen">
-					<AboutScreen />
-				</div>
-				<div className="screen">
-					<SkillsScreen />
-				</div>
-				<div className="screen">
-					<WorksScreen />
-				</div>
-				<div className="screen">
-					<ContactScreen />
-				</div> */}
 			</div>
 		</Layout>
 	);
 };
 
 const query = graphql`
-	query {
+	{
 		markdownRemark(fileAbsolutePath: { regex: "/menu/" }) {
 			frontmatter {
 				menuList {
